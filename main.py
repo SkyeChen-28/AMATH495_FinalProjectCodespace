71cb15a58e652bf7cc74c3d28bfe800e724184a9
SkyeChen-28 <skyechen28@gmail.com>
2023-04-12 20:47:26 -0400

    Added single plots, helper functions for more cohesion

--- main.py
+++ main.py
@@ -1,54 +1,87 @@
 import os
+import json
 import numpy as np
 import scipy as sp
 from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
 from src.odes import ClimateODEs
+from src.plotting import single_plot, all_plots
+from src.constants import DAYS_PER_YEAR
 
-def main():
-    # CLI Values
-    output_dir = "outputs"
+def json_to_dict(filepath: str) -> dict:
+    with open(filepath) as fp:
+        data = json.load(fp)
+    return data
+
+def check_ICs(filepath: str) -> tuple:
     
-    # Initial values
-    r = 1
-    p = 0.01 # GDP of poorer country relative to rich
-    I_r = 1
-    I_p = 1
-    c = 1
+    # Load in the json file
+    ICs_dict = json_to_dict(filepath)
+    
+    # Extract the variables
+    r = ICs_dict["r"]
+    p = ICs_dict["p"]
+    c = ICs_dict["c"]
+    
+    # Enforce variable conditions
+    assert r > 0, f"r = {r}: GDP (r) should be a strictly positive float. Please change the value of `r` in \"{filepath}\" such that r > 0"
+    assert p > 0, f"p = {p}: GDP (p) should be a strictly positive. Please change the value of `p` in \"{filepath}\" such that p > 0"
+    assert c >= 0, f"c = {c}: CO2 concentration (c) should be a non-negative float. Please change the value of `c` in \"{filepath}\" such that c >= 0"
+    
+    return r, p, c
+
+def check_params(filepath: str) -> tuple:
+    
+    # Load in the json file
+    params_dict = json_to_dict(filepath)
+    
+    # Extract the variables
+    G_r = params_dict["G_r"]
+    G_p = params_dict["G_p"]
+    K_r = params_dict["K_r"]
+    K_p = params_dict["K_p"]
+    gamma = params_dict["gamma"]
+    alpha = params_dict["alpha"]
+    beta = params_dict["beta"]
+    D = params_dict["D"] # Natural diaster intensity
+    f = params_dict["f"] # Disaster frequency: A diaster happens once every f year(s)
+    
+    # Enforce variable conditions
+    assert K_r > 0, f"K_r = {K_r}: Carrying capacity (K_r) should be a strictly positive float. Please change the value of `K_r` in \"{filepath}\" such that `K_r` > 0"
+    assert K_p > 0, f"K_p = {K_p}: Carrying capacity (K_p) should be a strictly positive float. Please change the value of `K_p` in \"{filepath}\" such that `K_p` > 0"
+    assert gamma > 0, f"gamma = {gamma}: CO2 Emission rate (gamma) should be a strictly positive float. Please change the value of `gamma` in \"{filepath}\" such that `gamma` > 0"
+    assert alpha > 0, f"alpha = {alpha}: CO2 Emission efficiency (alpha) should be a strictly positive float. Please change the value of `alpha` in \"{filepath}\" such that `alpha` > 0"
+    assert beta > 0, f"beta = {beta}: CO2 Emission rate (beta) should be a strictly positive float. Please change the value of `beta` in \"{filepath}\" such that `beta` > 0"
+    
+    return G_r, G_p, K_r, K_p, gamma, alpha, beta, D, f
+       
+
+def run_model(output_dir: str, ICs_filepath: str, params_filepath: str, num_years: int) -> None:
+        
+    # Load in initial values
+    r, p, c = check_ICs(ICs_filepath)
+    I_r = r
+    I_p = p
     y0 = np.array([r, p, I_r, I_p, c]) # Model is normalized so that all values are in terms of present day values
     
-    # Set system parameter values
-    G_r = 3e-3
-    G_p = 9e-4
-    K_r = 1.1
-    K_p = 0.7
-    gamma = 1
-    alpha = 1
-    beta = 1
-    climate = ClimateODEs(G_r, G_p, K_r, K_p, gamma, alpha, beta)
-    D = 1e-5 # Natural diaster intensity
-    f = 3 # Disaster frequency: A diaster happens once every f year(s)
-    
-    # Define timeframe
-    days_per_year = 365
-    num_years = 80
-    # num_years = 40
+    # Load in system parameter values
+    G_r, G_p, K_r, K_p, gamma, alpha, beta, D, f = check_params(params_filepath)
+    climate = ClimateODEs(G_r, G_p, K_r, K_p, gamma, alpha, beta, D, f)
     
     # Run model year by year
     t = np.array([0])
     y = y0.reshape((5, 1))
     for year in range(num_years + 1):
-        t_begin = year * days_per_year
-        t_end = t_begin + days_per_year
+        t_begin = year * DAYS_PER_YEAR
+        t_end = t_begin + DAYS_PER_YEAR
         t_span = (t_begin, t_end)
-        t_eval = np.linspace(t_begin, t_end, days_per_year + 1)
+        t_eval = np.linspace(t_begin, t_end, DAYS_PER_YEAR + 1)
     
         # Solve the ODE system
         result = solve_ivp(climate.vec_de, t_span, y[:,-1], t_eval=t_eval)
 
         # Parse results
-        t = np.append(t, result.t, axis = 0)
-        y = np.append(y, result.y, axis = 1)
+        t = np.append(t, result.t[1:], axis = 0)
+        y = np.append(y, result.y[:,1:], axis = 1)
         r = y[0]
         p = y[1]
         I_r = y[2]
@@ -56,76 +89,71 @@ def main():
         c = y[4]
         
         # Natural Disaster
-        if ((year != 0) and (year % f == 0)):
-            y[0,-1] -= 0.9 * r[-1] * (1 - np.exp(- D*c[-1] / r[-1])) # Reduces r
-            y[1,-1] -= 0.9 * p[-1] * (1 - np.exp(- D*c[-1] / p[-1])) # Reduces p
-    
-    
-    # Plot results
-    fig, axs = plt.subplots(3, 1) # Landscape plots
-    # fig, axs = plt.subplots(1, 3) # Portrait plots
-    
-    # Common plot parameters
-    x_axis_title = "Time (years)"
-    num_ticks = 8
-    xticks = np.arange(0, num_years+1, num_years//num_ticks) * days_per_year
-    xlabels = np.arange(0, num_years+1, num_years//num_ticks)
-    RICH_COUNTRY = "Rich Country"
-    POOR_COUNTRY = "Poor Country"
-    rich_col = "lime"
-    poor_col = "red"
-    poor_linestyle = "solid" #"dashed"
-    glob_col = "forestgreen"
-    
-    # Modifications common to all plots
-    for ax in axs:
-        ax.set_xlabel(x_axis_title)
-        ax.set_xticks(xticks, labels = xlabels)
-        ax.minorticks_on()
-        ax.grid(True, which = 'minor', color = '0.9')
-        ax.grid(True, which = 'major', color = 'darkgrey')
+        y[0,-1] -= climate.natural_disaster(year, f, r[-1], c[-1]) # Reduces r
+        y[1,-1] -= climate.natural_disaster(year, f, p[-1], c[-1]) # Reduces p
         
-    # Define indices
-    GDP_idx = 0
-    innovation_idx = 1
-    CO2_idx = 2
+    single_plot(t, r, 
+                output_dir = output_dir,
+                filename = "Rich_GDP.png",
+                num_years = num_years,
+                plt_title = "GDP of richer country over time", 
+                y_axis_title = "GDP relative to initial", 
+                xlims = 0, 
+                ylims = None
+                )
+    single_plot(t, p, 
+                output_dir = output_dir,
+                filename = "Poor_GDP.png",
+                num_years = num_years,
+                plt_title = "GDP of poorer country over time", 
+                y_axis_title = "GDP relative to richer's initial GDP", 
+                xlims = 0, 
+                ylims = 0,
+                )
+    single_plot(t, I_r, 
+                output_dir = output_dir,
+                filename = "Rich_Inno.png",
+                num_years = num_years,
+                plt_title = "Innovation of richer country over time", 
+                y_axis_title = "Innovation", 
+                xlims = 0, 
+                ylims = I_r[0]
+                )
+    single_plot(t, I_p, 
+                output_dir = output_dir,
+                filename = "Poor_Inno.png",
+                num_years = num_years,
+                plt_title = "Innovation of poorer country over time", 
+                y_axis_title = "Innovation", 
+                xlims = 0, 
+                ylims = I_p[0]
+                )
+    single_plot(t, c, 
+                output_dir = output_dir,
+                filename = "CO2.png",
+                num_years = num_years,
+                plt_title = r"CO$_2$ over time", 
+                y_axis_title = r"CO$_2$ Concentration", 
+                xlims = 0, 
+                ylims = 0
+                )
+    all_plots(t, y, num_years, output_dir)
     
-    # Plot GDPs
-    axs[GDP_idx].plot(t, r, label = RICH_COUNTRY, color = rich_col)
-    axs[GDP_idx].plot(t, p, label = POOR_COUNTRY, color = poor_col, linestyle = poor_linestyle)
-    axs[GDP_idx].set_title("GDP over time")
-    axs[GDP_idx].set_ylabel("GDP")
-    axs[GDP_idx].set_xlim(0)
-    axs[GDP_idx].set_ylim(0)
+
+def main():
+    # CLI Values
+    output_dir = "outputs"
+    ICs_filepath = "params/initial_conditions.json"
+    params_filepath = "params/ODE_params.json"
     
-    # Plot innovation
-    axs[innovation_idx].plot(t, I_r, label = RICH_COUNTRY, color = rich_col)
-    axs[innovation_idx].plot(t, I_p, label = POOR_COUNTRY, color = poor_col, linestyle = poor_linestyle)
-    axs[innovation_idx].set_title("Innovation over time")
-    axs[innovation_idx].set_ylabel("Innovation")
-    axs[innovation_idx].set_xlim(0)
-    axs[innovation_idx].set_ylim(0)
+    # Define timeframe
+    # num_years = 80
+    num_years = 40
     
-    # Plot CO2 concentration
-    axs[CO2_idx].plot(t, c, label = "Global", color = glob_col)
-    axs[CO2_idx].set_title(r"CO$_2$ over time")
-    axs[CO2_idx].set_ylabel(r"CO$_2$ Concentration")  
-    axs[CO2_idx].set_xlim(0)
-    axs[CO2_idx].set_ylim(0)
-    
-    # More common mods after labels have been added
-    for ax in axs:
-        ax.legend()
-    
-    # Show the plot
-    if (not os.path.exists(output_dir)):
-        os.mkdir(output_dir)
-    fig.tight_layout(rect = (0,0,0.95,1), h_pad = -1)
-    fig.set_size_inches((8.5, 11), forward = False)
-    fig.savefig(f'{output_dir}/plots.png', dpi = 500)
-    fig.show()
-    print()
-    
+    run_model(output_dir = output_dir, 
+              ICs_filepath = ICs_filepath, 
+              params_filepath = params_filepath, 
+              num_years = num_years)
 
 if __name__ == "__main__":
     main()
